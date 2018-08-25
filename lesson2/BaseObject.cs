2e0dfbf19d18d80f09bfbd859c6db37a005af412
fdhfgdh <fggh@yandex.ru>
2018-08-25 04:42:32 +0300

    х

---
+++ lesson2/BaseObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+
+namespace Asteroids
+{
+    abstract class BaseObject : ICollision
+    {
+        protected Point Pos;
+        protected Point Dir;
+        protected Size Size;
+
+        public delegate void Message();
+
+        protected BaseObject(Point pos, Point dir, Size size)
+        {
+            Pos = pos;
+            Dir = dir;
+            Size = size;
+        }
+
+        public Rectangle Rect => new Rectangle(Pos, Size);
+
+        public bool Collision(ICollision obj) => obj.Rect.IntersectsWith(this.Rect);
+
+        public virtual void Draw()
+        {
+            Game.Buffer.Graphics.DrawEllipse(Pens.White, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+
+        public abstract void Update();
+    }
+}
